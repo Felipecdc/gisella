b3192790f175662941a132941df4d885725c0302
Felipecdc <felipecdc09@gmail.com>
2026-05-21 22:30:26 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
+
+  basePath: "/gisella",
+  assetPrefix: "/gisella",
 };
 
 export default nextConfig;
